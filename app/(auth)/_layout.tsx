ba31d9e6a2bbe515529f1ba45d0942f983cbc8a0
Prakash <developer@Prakash-MacBook-Pro.local>
2024-10-17 15:53:41 +0530

    feat:filter tags implemented to filter the list

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack, Tabs } from "expo-router";
+import React from "react";
+
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function AuthLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="signin" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
